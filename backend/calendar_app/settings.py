e728cb519382af25927dd5c5071eab4cd156af12
EsterIso <EsterlinjPaulino@gmail.com>
2024-08-28 15:22:35 -0400

    Frontend calls

--- backend/calendar_app/settings.py
+++ backend/calendar_app/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'calendar_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('VITE_DATABASE_URL'))
 }
 
 # Password validation
