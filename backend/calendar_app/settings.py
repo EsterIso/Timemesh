d2a7d547902932e24e6bd421b2b73b1d196aee38
EsterIso <EsterlinjPaulino@gmail.com>
2024-08-28 01:12:02 -0400

    fixing settings

--- backend/calendar_app/settings.py
+++ backend/calendar_app/settings.py
@@ -17,6 +17,7 @@ Note:
 import os
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,10 +94,7 @@ WSGI_APPLICATION = 'calendar_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
