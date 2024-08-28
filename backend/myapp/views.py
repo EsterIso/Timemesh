f6796f130c4f2fb79d9e9275fce24129720e42f9
EsterIso <EsterlinjPaulino@gmail.com>
2024-08-28 04:08:18 -0400

    views

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,10 +1,12 @@
 from django.apps import apps
 from django.http import HttpResponse
 from django.core.management import call_command
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 def run_migrations(request):
     try:
-        call_command('makemigrations')  # This line was corrected
+        call_command('makemigrations')
         call_command('migrate')
         return HttpResponse("Migrations completed successfully.")
     except Exception as e:
@@ -12,17 +14,18 @@ def run_migrations(request):
 
 def create_superuser(request):
     try:
-        CustomUser = apps.get_model('users', 'CustomUser')
-        email = 'esterlinjpaulinp@gmail.com'  # Set your desired email
+        # Use the appropriate user model
+        User = get_user_model()
+        email = 'esterlinjpaulino@gmail.com'  # Set your desired email
         username = 'Ester'  # Set your desired username
         password = 'admin-app123'  # Set your desired password
 
-        # Check if the user already exists
-        if CustomUser.objects.filter(email=email).exists():
+        # Check if the superuser already exists
+        if User.objects.filter(email=email).exists() or User.objects.filter(username=username).exists():
             return HttpResponse("Superuser already exists.")
 
         # Create the superuser
-        CustomUser.objects.create_superuser(email=email, username=username, password=password)
+        User.objects.create_superuser(email=email, username=username, password=password)
         
         return HttpResponse("Superuser created successfully.")
     except Exception as e:
