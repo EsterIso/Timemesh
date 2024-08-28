04049c82290eab243b7963bb8ec8d39ad51d3b1d
EsterIso <EsterlinjPaulino@gmail.com>
2024-08-28 17:27:08 -0400

    json

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+set -x  # Enable debugging output
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
+echo "Contents of staticfiles directory:"
+ls -R staticfiles
