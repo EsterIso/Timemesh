efe94a561c84c494a50a864bc071468220bcdeb0
EsterIso <EsterlinjPaulino@gmail.com>
2024-08-28 02:03:12 -0400

    fixed views

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -10,7 +10,17 @@ def run_migrations(request):
 
 def create_superuser(request):
     try:
-        call_command('createsuperuser', interactive=False)
+        username = 'Ester'  # Set your desired username
+        email = 'esterlinjpaulinp@gmail.com'  # Set your desired email
+        password = 'admin-app123'  # Set your desired password
+
+        # Using Django’s built-in create_superuser method to avoid interactive prompts
+        call_command('createsuperuser', 
+                     interactive=False, 
+                     username=username, 
+                     email=email, 
+                     password=password)
+        
         return HttpResponse("Superuser created successfully.")
     except Exception as e:
         return HttpResponse(f"An error occurred: {e}")
